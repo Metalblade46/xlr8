0e133cae8b6fca6f1fb749056fb027fb0051855b
SabbyasachiChakraborty <chakrabortysabbyasachi@gmail.com>
2025-10-27 08:05:42 +0530

    Refactor TRPC context and add protected procedure
    Replaces the TRPC context creation to use authentication and session data, introduces a protectedProcedure for authenticated queries, and updates the app router to provide a getMyUser endpoint requiring authentication. Also updates Sentry config to enable logs only in production.

--- trpc/init.ts
+++ trpc/init.ts
@@ -1,16 +1,20 @@
-import { initTRPC } from '@trpc/server';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
 import { cache } from 'react';
-export const createTRPCContext = cache(async () => {
-    /**
-     * @see: https://trpc.io/docs/server/context
-     */
-    return { userId: 'user_123' };
+import { initTRPC, TRPCError } from '@trpc/server';
+export const createContext = cache(async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    return { auth: session };
 });
+export type Context = Awaited<ReturnType<typeof createContext>>;
+
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<Context>().create({
     /**
      * @see https://trpc.io/docs/server/data-transformers
      */
@@ -20,3 +24,9 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(async ({ ctx, next }) => {
+    if (!ctx.auth?.session.token) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+    return next({ ctx: { ...ctx, auth: ctx.auth } });
+});
--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,9 +1,8 @@
-
-import { baseProcedure, createTRPCRouter } from '../init';
+import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/prisma';
 export const appRouter = createTRPCRouter({
-    getUsers: baseProcedure
-        .query(() => prisma.user.findMany()),
+    getMyUser: protectedProcedure
+        .query(({ ctx }) => prisma.user.findFirst({ where: { id: ctx.auth.user.id } })),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
